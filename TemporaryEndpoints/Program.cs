9b28253fa0a0522fce134a03b77a939cbe7b4ee6
Ostap <harasymostap@gmail.com>
2022-12-28 19:56:02 +0200

    initial commit

---
+++ TemporaryEndpoints/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Data
+var lots = new List<LotItem>
+{
+    new LotItem(1, "Painting", 174),
+    new LotItem(2, "Car", 17434),
+    new LotItem(3, "Horse", 3174),
+    new LotItem(4, "Chair", 74),
+    new LotItem(5, "Carpet", 17),
+};
+
+// Endpoints
+app.MapGet("/lots", () => lots);
+
+app.MapGet("/lots/{id}", (int id) => lots.First(x => x.Id == id));
+
+app.MapPost("/lots", (CreateLotRequest request) =>
+{
+    var newLot = new LotItem(lots.Max(x => x.Id), request.Title, request.CurrentBid);
+    lots.Add(newLot);
+    return newLot;
+});
+
+app.MapPut("/lots", (LotItem request) =>
+{
+    var updatedLot = lots.First(x => x.Id == request.Id);
+    lots.Remove(updatedLot);
+    lots.Add(request);
+    return request;
+});
+
+app.MapDelete("/lots/{id}", (int id) =>
+{
+    var deletedLot = lots.First(x => x.Id == id);
+    lots.Remove(deletedLot);
+    return Results.NoContent();
+});
+
+app.Run();
+
+// Types
+record LotItem(int Id, string Title, decimal CurrentBid);
+record CreateLotRequest(string Title, decimal CurrentBid);
