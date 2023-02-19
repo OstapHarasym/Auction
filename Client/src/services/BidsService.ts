cb15a9b156457a92e9226c3fafa6d1f63ea39031
Ostap Harasym <harasymostap@gmail.com>
2023-02-19 23:18:40 +0200

    fix build

--- Client/src/services/BidsService.ts
+++ Client/src/services/BidsService.ts
@@ -3,6 +3,6 @@ import {AddBidModel} from '../models/AddBidModel';
 import {BidModel} from '../models/BidModel';
 
 export const BidsService = {
-  getBids: (lotId: string): Promise<BidModel[]> => requests.get(`/bids?lotId=${lotId}`),
+  getBids: (lotId: string): Promise<BidModel[]> => requests.get(`/bids?lotId=${lotId}`, {}),
   addBid: (bid: AddBidModel) => requests.post('/bids', bid)
 }
--- Client/src/services/LotsService.ts
+++ Client/src/services/LotsService.ts
@@ -4,8 +4,8 @@ import {CreateLotModel} from '../models/CreateLotModel';
 import {CatalogueModel} from '../models/CatalogueModel';
 
 export const LotsService = {
-  getLots: (): Promise<CatalogueModel> => requests.get('/lots'),
-  getLot: (id: string): Promise<CatalogueItemModel> => requests.get(`/lots/${id}`),
+  getLots: (): Promise<CatalogueModel> => requests.get('/lots', {}),
+  getLot: (id: string): Promise<CatalogueItemModel> => requests.get(`/lots/${id}`, {}),
   createLot: (lot: CreateLotModel) => requests.post('/lots', lot),
   deleteLot: (id: string) => requests.delete(`/lots/${id}`)
 }
